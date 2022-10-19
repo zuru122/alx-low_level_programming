284c07f5050ad3582f5d5eebceca9d7ba9ee8bf7
Justice <onyekaozuru122@gmail.com>
2022-10-19 14:05:44 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - a function that print 10 times the alphabet
- * Return x10 a-z
+ * main - a function that print 10 times the alphabet
+ * Return: 0;
  */
 void print_alphabet_x10(void)
 {
