b88fb93e44ddbc0d9a23bb7388c24feda2edc988
Justice <onyekaozuru122@gmail.com>
2022-10-19 14:49:51 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,7 +26,7 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (prod >= 100)
 				{
-					_putchar((pro / 100) + '0');
+					_putchar((prod / 100) + '0');
 					_putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
@@ -35,7 +35,7 @@ void print_times_table(int n)
 				}
 				_putchar((prod % 10) + '0');
 			}
-			_putchae('\n');
+			_putchar('\n');
 		}
 	}
 }
