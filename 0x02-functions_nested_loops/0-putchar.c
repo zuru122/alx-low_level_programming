37b97f881ff22af684bb7dedd28a1f1378f197e0
Justice <onyekaozuru122@gmail.com>
2022-10-19 02:50:01 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+	return (0);
+}
