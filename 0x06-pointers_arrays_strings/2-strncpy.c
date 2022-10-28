bcd9f4bef1c82532aad5454f1298bebb797f3344
Justice <onyekaozuru122@gmail.com>
2022-10-28 15:09:04 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - Function that copies a string.
+ * @dest: pointer to destination char
+ * @src: pointer to source char
+ * @n: number of bytes
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src. int n)
+{
+	int i = 0;
+
+	while (i < n && (src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++
+	}
+	while (i != n)
+		dest[i++] = '\0';
+	return (dest);
+}
