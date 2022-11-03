56309e71705a08cfd546bbed069087344ec935ca
Justice <onyekaozuru122@gmail.com>
2022-11-03 09:14:15 -0700

    Task 0 on recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: string to receive
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
