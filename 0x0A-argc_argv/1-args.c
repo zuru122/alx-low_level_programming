019b4cf0053dc4bd4d346304c2dcf86ff25f2154
Justice <onyekaozuru122@gmail.com>
2022-11-08 05:45:13 -0800

    argc, argv Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program that prints the number of argument
+ * passed into it
+ * @argc: argument count
+ * @argv: argumnet vector
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		argv = argv;
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
