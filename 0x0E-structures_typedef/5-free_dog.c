8969bc84a9d7ef780cadfd501b6236b4b7adbfb2
Justice <onyekaozuru122@gmail.com>
2022-11-15 12:56:34 -0800

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - Entry Point
+ * @d: struct to free
+ * Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d != 0)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
