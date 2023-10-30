c8984b999625bca1069fc1e7a4e806efaf7fad77
Justice <onyekaozuru122@gmail.com>
2023-10-30 05:55:07 -0700

    fixing code to meet betty standard

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,26 +11,25 @@
  */
 void print_binary(unsigned long int n)
 {
-    int shift = sizeof(unsigned long int) * 8 - 1;
-    int flag = 0;
+	int shift = sizeof(unsigned long int) * 8 - 1;
+	int flag = 0;
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
-
-    while (shift >= 0)
-    {
-        if ((n >> shift) & 1)
-        {
-            flag = 1;
-            _putchar('1');
-        }
-        else if (flag)
-        {
-            _putchar('0');
-        }
-        shift--;
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (shift >= 0)
+	{
+		if ((n >> shift) & 1)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag)
+		{
+			_putchar('0');
+		}
+		shift--;
+	}
 }
