c409c75f67a00d88aaab61a06629af3c41745930
Justice <onyekaozuru122@gmail.com>
2023-10-30 05:45:58 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+/*
+ * Author: Sunday, Justice Gabriel
+ * Task: 0x14. C - Bit manipulation "1-print_binary.c"
+ */
+
+#include "main.h"
+/**
+ * print_binary - convert number to binary
+ * @n: positive number to be converted
+ * Return: Null, else the converted decimal to binary
+ */
+void print_binary(unsigned long int n)
+{
+    int shift = sizeof(unsigned long int) * 8 - 1;
+    int flag = 0;
+
+    if (n == 0)
+    {
+        _putchar('0');
+        return;
+    }
+
+    while (shift >= 0)
+    {
+        if ((n >> shift) & 1)
+        {
+            flag = 1;
+            _putchar('1');
+        }
+        else if (flag)
+        {
+            _putchar('0');
+        }
+        shift--;
+    }
+}
